0d9e5a8b0abb6be26ab00a7737b75ec6618f8606
Lisbeth Diaz <lizbeths.robles@gmail.com>
2024-03-28 22:29:11 -0400

    Cuarto curso agregado
    Se repararon algunos links

--- src/data.js
+++ src/data.js
@@ -9,14 +9,14 @@ export const cursos = [
           numero: 1,
           tareas: [
             // https://diazsa.blob.core.windows.net/coursera/El pensamiento de la Inteligencia artificial _ Coursera.pdf
-            { id: 1, nombre: "El pensamiento de la Inteligencia artificial", link: './assets/archive/https://diazsa.blob.core.windows.net/coursera/El pensamiento de la Inteligencia artificial _ Coursera.pdf' },
+            { id: 1, nombre: "El pensamiento de la Inteligencia artificial", link: './assets/archive/https://diazsa.blob.core.windows.net/coursera/%20pensamiento%20de%20la%20Inteligencia%20artificial%%20Coursera.pdf' },
            
           ],
         },
         {
           numero: 2,
           tareas: [
-            { id: 2, nombre: "Conceptos básicos de Inteligencia artificial", link:'https://diazsa.blob.core.windows.net/coursera/Conceptos básicos de Inteligencia artificial _ Coursera.pdf'},
+            { id: 2, nombre: "Conceptos básicos de Inteligencia artificial", link:'https://diazsa.blob.core.windows.net/coursera/Conceptos%20básicos%20de%20Inteligencia%20artificial%20_%20Coursera.pdf'},
           ],
         },
         {
@@ -34,42 +34,42 @@ export const cursos = [
         {
           numero: 1,
           tareas: [
-            { id: 4, nombre: "Logica Proporcional", link:'https://diazsa.blob.core.windows.net/coursera/Lógica proposicional _ Coursera.pdf' },
+            { id: 4, nombre: "Logica Proporcional", link:'https://diazsa.blob.core.windows.net/coursera/Lógica%20proposicional%20_%20Coursera.pdf' },
            
           ],
         },
         {
           numero: 2,
           tareas: [
-            { id: 5, nombre: "Logica Proporcional (parte 2)", link:'https://diazsa.blob.core.windows.net/coursera/Lógica proposicional (parte 2) _ Coursera.pdf' },
+            { id: 5, nombre: "Logica Proporcional (parte 2)", link:'https://diazsa.blob.core.windows.net/coursera/Lógica%20proposicional%20(parte%202)%20_%20Coursera.pdf' },
             { id:6 , nombre: "Algoritmo DPLL", link:'https://diazsa.blob.core.windows.net/coursera/Programing_Logica_proporcional2.txt'}
           ],
         },
         {
             numero: 3,
             tareas: [
-              { id: 7, nombre: "Logica Temporal", link:'https://diazsa.blob.core.windows.net/coursera/Lógica temporal _ Coursera.pdf' },
-              { id: 8, nombre: "Logica Predicados", link:'https://diazsa.blob.core.windows.net/coursera/Lógica de predicados _ Coursera.pdf' },
+              { id: 7, nombre: "Logica Temporal", link:'https://diazsa.blob.core.windows.net/coursera/Lógica%20temporal%20_%20Coursera.pdf' },
+              { id: 8, nombre: "Logica Predicados", link:'https://diazsa.blob.core.windows.net/coursera/Lógica%20de%20predicados%20_%20Coursera.pdf' },
             ],
         },
         {
             numero: 4,
             tareas: [
-                { id: 9, nombre: "Teorema de Bayes", link:'https://diazsa.blob.core.windows.net/coursera/Teorema de Bayes _ Coursera.pdf' },
+                { id: 9, nombre: "Teorema de Bayes", link:'https://diazsa.blob.core.windows.net/coursera/Teorema%20de%20Bayes%20_%20Coursera.pdf' },
                 { id: 10, nombre: "Redes Bayesiana", link:'https://diazsa.blob.core.windows.net/coursera/redes_bayesianas.txt' },
             ],
         },
         {
             numero: 5,
             tareas: [
-                { id: 9, nombre: "Teoria de Deciciones", link:'https://diazsa.blob.core.windows.net/coursera/Teoría de decisiones _ Coursera.pdf' },
+                { id: 9, nombre: "Teoria de Deciciones", link:'https://diazsa.blob.core.windows.net/coursera/Teoría%20de%20decisiones%20_%20Coursera.pdf' },
                 { id: 10, nombre: "Teoria de Deciciones Programing", link:'https://diazsa.blob.core.windows.net/coursera/Teoria_Deciciones.txt' },
             ],
         },
         {
             numero: 6,
             tareas: [
-                { id: 11, nombre: "Teoria de Juegos", link:'https://diazsa.blob.core.windows.net/coursera/Teoría de juegos _ Coursera.pdf' },
+                { id: 11, nombre: "Teoria de Juegos", link:'https://diazsa.blob.core.windows.net/coursera/Teoría%20de%20juegos%20_%20Coursera.pdf' },
                 { id: 12, nombre: "Teoria de Deciciones Programing", link:'https://diazsa.blob.core.windows.net/coursera/Decision.txt' },
             ],
         },
@@ -82,7 +82,7 @@ export const cursos = [
           {
             numero: 1,
             tareas: [
-              { id: 13, nombre: "Algoritmos de búsqueda ciega", link:'https://diazsa.blob.core.windows.net/coursera/Algoritmos de búsqueda ciega _ Coursera.pdf' },
+              { id: 13, nombre: "Algoritmos de búsqueda ciega", link:'https://diazsa.blob.core.windows.net/coursera/Algoritmos%20de%20búsqueda%20ciega%20_%20Coursera.pdf' },
              
             ],
           },
@@ -108,10 +108,47 @@ export const cursos = [
           {
               numero: 5,
               tareas: [
-                  { id: 9, nombre: "Algoritmos metaheurísticos", link:'https://diazsa.blob.core.windows.net/coursera/Algoritmos metaheurísticos _ Coursera.pdf' },
+                  { id: 9, nombre: "Algoritmos metaheurísticos", link:'https://diazsa.blob.core.windows.net/coursera/Algoritmos%20metaheurísticos%20_%20Coursera.pdf' },
                   { id: 17, nombre: "Evaluación final: Solución del cubo de Rubik", link:'https://coursera-assessments.s3.amazonaws.com/assessments/1708390364280/479a38b4-52cd-4af4-a762-cadc78578db2/Tarea%20Final%20-%20relucion%20problemas%20busqueda%20-%20Final.pdf' },
               ],
           },
+          
+         
+        ],
+      },
+      {
+        id: 4,
+        nombre: "Computo Evolutivo",
+        semanas: [
+          {
+            numero: 1,
+            tareas: [
+              { id: 13, nombre: "Conceptos de algoritmos genéticos", link:'https://diazsa.blob.core.windows.net/coursera/Conceptos%20de%20algoritmos%20genéticos%20_%20Coursera.pdf' },
+             
+            ],
+          },
+          {
+            numero: 2,
+            tareas: [
+              { id: 14, nombre: "Estudio del Teorema del Esquema", link:'https://diazsa.blob.core.windows.net/coursera/Estudio%20del%20Teorema%20del%20Esquema%20_%20Coursera.pdf' },
+              { id: 15, nombre: "Análisis comparativo de codificaciones en algoritmos genéticos.", link:'https://coursera-assessments.s3.amazonaws.com/assessments/1708729565569/82636b25-29fb-4c11-989d-80b8d3162679/An%C3%A1lisis%20comparativo%20de%20codificaciones%20en%20algoritmos%20gen%C3%A9ticos.pdf' },
+              
+            ],
+          },
+          {
+              numero: 3,
+              tareas: [
+                { id: 16, nombre: "Ruta óptima del TSP", link:'https://diazsa.blob.core.windows.net/coursera/rutatspQuiz.txt' },
+                
+              ],
+          },
+          {
+              numero: 4,
+              tareas: [
+                  { id: 17, nombre: "Solución al Problema del Agente Viajero", link:'https://coursera-assessments.s3.amazonaws.com/assessments/1708991570108/bf17bb69-e665-4549-a925-871cb7927831/Actividadfinal_M4.pdf' },
+              ],
+          },
+        
           
          
         ],
